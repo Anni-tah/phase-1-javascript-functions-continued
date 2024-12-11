eaa2852dc2a3cacab7979a38452261742e866c3f
Anne Nabwire <nabanne96@gmail.com>
2024-12-11 11:14:22 +0300

    lab completed'

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity ="roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(work = 'go to the office'){
+return (`This Monday, I will ${work}.`);
+
+}
+mondayWork('work from home');
+
+function wrapAdjective( wrap = '*'){
+    const empatic = function(special){
+        return (`You are ${wrap}${special}${wrap}!`);
+    };
+return empatic;
+}
+wrapAdjective("%")("a dedicated programmer");
